b14512b80b6a722b2b44cdb57ceaad097e9b1d13
Ben Evans <ben@bensbit.co.uk>
2013-05-09 14:40:01 +0100

    add tests

---
+++ test.js
@@ -0,0 +1,23 @@
+
+var assert = require('assert'),
+    fs = require('fs'),
+    spawn = require('child_process').spawn;
+
+describe('jon', function() {
+  it('should error if no path provided', function(done) {
+    var jon = spawn('bin/jon');
+    jon.on('close', function (code) {
+      assert.equal(code, 1);
+      done();
+    });
+  });
+  it('should error if path provided resolves to no file', function(done) {
+    var jon = spawn('bin/jon', ['no-file-exists-path.json']);
+    jon.on('close', function (code) {
+      assert.equal(code, 1);
+      done();
+    });
+  });
+  it('should get and print in valid JSON a dot-notation attribute');
+  it('should update and print in valid JSON a dot-notation attribute');
+});
